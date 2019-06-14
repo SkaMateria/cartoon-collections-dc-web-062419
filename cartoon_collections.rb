b43793c08b821eb883268d3b79ab4cf369728606
Carlos Velazquez <skamateria@gmail.com>
2019-06-14 13:51:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,5 @@
-dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
-
 def roll_call_dwarves(array)
-  array.each_with_index | name, index|
+  array.each_with_index do | name, index|
    puts "#{index + 1} #{name}"
  end
 end
